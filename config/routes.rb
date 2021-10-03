dc43393a4615f0a4e16f25f00f9f530668964890
gustavomendess <gustavocod13@outlook.com>
2021-10-03 20:35:15 -0300

    fix: route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'appointments#index'
-  get 'dashboards/search', to: 'dashboards#search', as: :search_dashboards
-  resources :dashboards, only: [:index]
+  resources :dashboards, only: [:index, :show]
   resources :doctors
   resources :patients
   resources :appointments
