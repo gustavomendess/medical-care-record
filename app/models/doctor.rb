b22400d430e0cfe9413ee3c824770eae3994a577
gustavomendess <gustavocod13@outlook.com>
2021-10-03 21:14:42 -0300

    fix: uniqueness validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,19 +1,13 @@
 class Doctor < ActiveRecord::Base
   has_many :appointment, dependent: :restrict_with_error
 
+  validates :crm, uniqueness: { scope: :crm_uf }
+
   validate :attributes_presence
-  validate :duplicated_doctor
 
   def attributes_presence
     errors.add(:base, I18n.t('errors.appointments.no_name')) if name.nil?
     errors.add(:base, I18n.t('errors.appointments.no_crm')) if crm.nil?
     errors.add(:base, I18n.t('errors.appointments.no_crm_uf')) if crm_uf.nil?
-  end
-
-  def duplicated_doctor
-    return unless new_record?
-
-    duplicated = Doctor.where(crm: crm, crm_uf: crm_uf).exists?
-    errors.add(:base, "O CRM (#{crm} - #{crm_uf}) já está em uso") if duplicated
   end
 end
